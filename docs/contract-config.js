f702441684875213644d476d9b97bc353bff21c2
minhthuphamhoang <141140249+minhthuphamhoang@users.noreply.github.com>
2026-07-28 21:25:30 +0700

    Add files via upload

---
+++ docs/contract-config.js
@@ -0,0 +1,7 @@
+window.TRADE_DAPP_CONFIG = {
+  contractAddress: "0x628eedc43f0832071d5454d20419ef0c8f1231d2",
+  chainIdHex: "0xaa36a7",
+  chainIdDecimal: 11155111,
+  chainName: "Sepolia",
+  explorerBaseUrl: "https://sepolia.etherscan.io"
+};
